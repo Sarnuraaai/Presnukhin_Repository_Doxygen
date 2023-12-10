7546b4c3c7e6e3fd573f8d317a3fb868efeedf98
Андрей Преснухин <andrey.presnukhin@gmail.com>
2023-12-10 20:24:32 +0300

    Commit

---
+++ pocket.bat
@@ -0,0 +1,4 @@
+"C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\
+VCExpress.exe" project_test.sln /build Debug
+"C:\Program Files\doxygen\bin\doxygen.exe" myConfig
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" build.iss
